5cc623a4fd2a86545b783037c70773d7ff760ede
Chris Marshall <chris@chrismar035.com>
2013-06-17 16:55:44 -0400

    Version 1.0

---
+++ CarPaymentCalculator/build/source/buildConfig/debug/com/chrismar035/carpaymentcalculator/BuildConfig.java
@@ -0,0 +1,7 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.chrismar035.carpaymentcalculator;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+
+}
